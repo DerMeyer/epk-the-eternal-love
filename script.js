5d567741c0046bf02cfa3b325f6d8bdfb5001c57
Der Meyer <simon.der.meyer@gmail.com>
2019-01-10 15:38:52 +0100

    Video and js.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 (function() {
 
-    console.log('Hi there!');
+    var imageOne = document.getElementById('image-one');
+    var imageTwo = document.getElementById('image-two');
+    var imgOne = document.getElementById('img-one');
+    var imgTwo = document.getElementById('img-two');
+
+    function toggleImages() {
+        imageOne.classList.toggle('image-button');
+        imageOne.classList.toggle('image-active');
+        imageTwo.classList.toggle('image-button');
+        imageTwo.classList.toggle('image-active');
+        imgOne.classList.toggle('img--small');
+        imgTwo.classList.toggle('img--small');
+        if (activeImage === 'one') {
+            activeImage = 'two';
+            imageTwo.addEventListener('click', toggleImages);
+            imageOne.removeEventListener('click', toggleImages);
+        } else {
+            activeImage = 'one';
+            imageOne.addEventListener('click', toggleImages);
+            imageTwo.removeEventListener('click', toggleImages);
+        }
+    }
+
+    var activeImage = 'two';
+    imageTwo.addEventListener('click', toggleImages);
 
 })();
